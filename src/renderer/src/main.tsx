60c8b2d4b8025aec9cb2adcd11acc4dba0ac6e2b
Joey Yu <joey@jyu.dev>
2023-10-28 15:26:55 -0400

    remove duplicated createRoot

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, Router } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
@@ -32,11 +30,3 @@ if (!rootElement.innerHTML) {
     </QueryClientProvider>
   )
 }
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-)
