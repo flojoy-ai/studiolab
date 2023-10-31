a44afc50bf20c7660409f3a78f5a5aaf0617415a
Joey Yu <joey@jyu.dev>
2023-10-31 16:29:57 -0400

    some updates

--- src/renderer/src/routeTree.gen.ts
+++ src/renderer/src/routeTree.gen.ts
@@ -1,7 +1,17 @@
 import { route as rootRoute } from "./routes/__root"
+import { route as IndexRoute } from "./routes/index"
 
 declare module "@tanstack/react-router" {
-  interface FileRoutesByPath {}
+  interface FileRoutesByPath {
+    "/": {
+      parentRoute: typeof rootRoute
+    }
+  }
 }
 
-export const routeTree = rootRoute.addChildren([])
+Object.assign(IndexRoute.options, {
+  path: "/",
+  getParentRoute: () => rootRoute,
+})
+
+export const routeTree = rootRoute.addChildren([IndexRoute])
--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,40 +1,13 @@
-import { Link, Outlet, RootRoute } from '@tanstack/react-router'
+import { Outlet, RootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const route = new RootRoute({
   component: (): JSX.Element => (
     <>
-      <div className="flex gap-2 p-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold'
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>{' '}
-        {/* <Link */}
-        {/*   to={'/posts'} */}
-        {/*   activeProps={{ */}
-        {/*     className: 'font-bold' */}
-        {/*   }} */}
-        {/* > */}
-        {/*   Posts */}
-        {/* </Link> */}
-        {/* <Link */}
-        {/*   to="/layout-a" */}
-        {/*   activeProps={{ */}
-        {/*     className: 'font-bold' */}
-        {/*   }} */}
-        {/* > */}
-        {/*   Layout */}
-        {/* </Link> */}
-      </div>
-      <hr />
       <Outlet />
-      {/* Start rendering router matches */}
       <TanStackRouterDevtools position="bottom-right" />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
     </>
   )
 })
