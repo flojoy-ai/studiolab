6efb52341d462352adbe57f3c288acca5de44c0f
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-11-01 15:18:44 -0400

    it works I guess idk untested

--- captain/controllers/flow.py
+++ captain/controllers/flow.py
@@ -1,125 +1,144 @@
 from typing import List, Literal, Tuple
 from pydantic import BaseModel
 import reactivex as rx
+from reactivex import Observable
 from reactivex.subject import BehaviorSubject
 import reactivex.operators as ops
 import itertools
 
 FLOWCHART = '{"blocks":[{"id":"0","block":"slider","ins":[],"outs":[{"target":"2","source":"1","sourceParam":"value","targetParam":"first"}]},{"id":"1","block":"slider","ins":[],"outs":[{"target":"2","source":"1","sourceParam":"value","targetParam":"second"}]},{"id":"2","block":"add","ins":[{"target":"2","source":"1","sourceParam":"value","targetParam":"first"},{"target":"2","source":"0","sourceParam":"value","targetParam":"second"}],"outs":[{"target":"3","source":"2","sourceParam":"value","targetParam":"value"}]},{"id":"3","block":"bignum","ins":[{"target":"3","source":"2","sourceParam":"value","targetParam":"value"}],"outs":[]},{"id":"Island2","block":"gamepad","ins":[],"outs":[]}]}'
 
+
 class FCBlockConnection(BaseModel):
-    target: str
-    source: str
-    sourceParam: str
-    targetParam: str
+  target: str
+  source: str
+  sourceParam: str
+  targetParam: str
+
 
 class FCBlock(BaseModel):
-    id: str
-    block: Literal['slider','gamepad','button','bignum','add','subtract']
-    ins: List[FCBlockConnection]
-    outs: List[FCBlockConnection]
+  id: str
+  block: Literal['slider', 'gamepad', 'button', 'bignum', 'add', 'subtract']
+  ins: List[FCBlockConnection]
+  outs: List[FCBlockConnection]
+
 
 class FlowChart(BaseModel):
-    blocks: List[FCBlock]
+  blocks: List[FCBlock]
 
 
 def slider(x):
-    print(f"slider: {x}")
-    return x
+  print(f"slider: {x}")
+  return x
+
 
 def gamepad(x):
-    print(f"gamepad: {x}")
-    return x
+  print(f"gamepad: {x}")
+  return x
+
 
 def button(x: bool):
-    print(f"button: {x}")
-    return x
+  print(f"button: {x}")
+  return x
+
 
 def bignum(x):
-    print(f"bignum: {x}")
-    return x
+  print(f"bignum: {x}")
+  return x
+
 
 def add(x, y):
-    print(f"add: {x} + {y}")
-    return x + y
+  print(f"add: {x} + {y}")
+  return x + y
+
 
 def subtract(x, y):
-    print(f"subtract: {x} - {y}")
-    return x - y
+  print(f"subtract: {x} - {y}")
+  return x - y
+
 
 FUNCTIONS = {
-    'slider': slider,
-    'gamepad': gamepad,
-    'button': button,
-    'bignum': bignum,
-    'add': add,
-    'subtract': subtract,
+  'slider': slider,
+  'gamepad': gamepad,
+  'button': button,
+  'bignum': bignum,
+  'add': add,
+  'subtract': subtract,
 }
 
-def build_flowchart(fc_json: str, start_observable: BehaviorSubject):
-    fc = FlowChart.model_validate_json(fc_json)
 
-    blocks = {block.id: block for block in fc.blocks}
-    islands = find_islands(blocks)
+def build_flowchart(fc_json: str, start_observable: BehaviorSubject, publish_fn=lambda x: None):
+  fc = FlowChart.model_validate_json(fc_json)
 
-    all_subjects = {}
-    graph_subjects = [build_graph(blocks, island, all_subjects) for island in islands]
+  blocks = {block.id: block for block in fc.blocks}
+  islands = find_islands(blocks)
 
-    in_subjects = [sub for blk, sub in itertools.chain(*graph_subjects) if len(blk.ins) == 0]
+  all_subjects = {}
+  graph_subjects = [build_graph(blocks, island, all_subjects) for island in islands]
 
-    def call_all_ins(_):
-        for subj in in_subjects:
-            # TODO: Figure out a better start value
-            subj.on_next(None)
+  in_subjects = [inp for blk, inp, outp in itertools.chain(*graph_subjects) if len(blk.ins) == 0]
 
-    start_observable.subscribe(on_next=call_all_ins)
-    return graph_subjects
+  def call_all_ins(_):
+    for subj in in_subjects:
+      # TODO: Figure out a better start value
+      subj.on_next(None)
 
-def build_graph(blocks: dict[str, FCBlock], island: list[FCBlock], subjects: dict[str, BehaviorSubject]):
-    terminals = list(filter(lambda b: len(b.outs) == 0, island))
+  start_observable.subscribe(on_next=call_all_ins)
 
-    def rec_build_graph(block: FCBlock) -> Tuple[FCBlock, rx.Observable]:
-        subject = subjects.get(block.id)
-        if subject is None:
-            subject = BehaviorSubject(None)
+  for _, out, blk in itertools.chain(*graph_subjects):
+    out.subscribe(publish_fn)
+
+  return graph_subjects
 
 
-        if len(block.ins) == 0:
-            return (block, subject)
+def build_graph(blocks: dict[str, FCBlock], island: list[FCBlock], subjects: dict[str, BehaviorSubject]) -> list[Tuple[FCBlock, BehaviorSubject, Observable]]:
+  terminals = list(filter(lambda b: len(b.outs) == 0, island))
 
-        in_subjects = [
-            rec_build_graph(blocks[edge.source])[1].pipe(ops.map(lambda x: (edge.targetParam, getattr(x, edge.sourceParam))))
-            for edge in block.ins
-        ]
-        zipped_in = rx.zip(*in_subjects).pipe(ops.map(lambda ins: {param: sub for param, sub in ins}))
-        zipped_in.subscribe(on_next = subject.on_next)
-        return (block, subject)
-    return [rec_build_graph(term) for term in terminals]
+  def rec_build_graph(block: FCBlock) -> Tuple[FCBlock, BehaviorSubject, Observable]:
+    input, output = subjects.get(block.id)
+    if input is None:
+      input = BehaviorSubject(None)
+      output = input.pipe(ops.map(lambda x: FUNCTIONS[block.block](**x)))
+
+    if len(block.ins) == 0:
+      return block, input, output
+
+    in_subjects = [
+      rec_build_graph(blocks[edge.source])[1].pipe(ops.map(lambda x: (edge.targetParam, getattr(x, edge.sourceParam))))
+      for edge in block.ins
+    ]
+    zipped_in = rx.zip(*in_subjects).pipe(ops.map(lambda ins: {param: sub for param, sub in ins}))
+    zipped_in.subscribe(on_next=lambda x: input.on_next(x))
+    return block, input, output
+
+  return [rec_build_graph(term) for term in terminals]
 
 
 def find_islands(blocks: dict[str, FCBlock]):
-    def dfs(block: FCBlock, visited: set[str], island: list[FCBlock]):
-        visited.add(block.id)
-        island.append(block)
-        for connection in [i.source for i in block.ins] + [o.target for o in block.outs]:
-            if connection in visited:
-                continue
-            dfs(blocks[connection], visited, island)
+  def dfs(block: FCBlock, visited: set[str], island: list[FCBlock]):
+    visited.add(block.id)
+    island.append(block)
+    for connection in [i.source for i in block.ins] + [o.target for o in block.outs]:
+      if connection in visited:
+        continue
+      dfs(blocks[connection], visited, island)
 
-    visited = set()
-    islands = []
-    for block in blocks.values():
-        if block.id not in visited:
-            island = []
-            dfs(block, visited, island)
-            islands.append(island)
-    return islands
+  visited = set()
+  islands = []
+  for block in blocks.values():
+    if block.id not in visited:
+      island = []
+      dfs(block, visited, island)
+      islands.append(island)
+  return islands
+
 
 def main():
-    build_flowchart(FLOWCHART)
+  build_flowchart(FLOWCHART)
+
 
 if __name__ == "__main__":
-    main()
+  main()
 
 # def build_node(fn, inputObservables, output_obs, render):
 #     def start_next_observable(x):
