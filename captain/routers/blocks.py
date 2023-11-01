92496cc950294b6327b428ef872dd0943cd72a5d
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-11-01 09:59:29 -0400

    hhiuuiubiuu hu

--- captain/routers/blocks.py
+++ captain/routers/blocks.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
+from reactivex import Subject
 
 router = APIRouter(tags=["blocks"], prefix="/blocks")
 
@@ -6,3 +7,17 @@ router = APIRouter(tags=["blocks"], prefix="/blocks")
 @router.get("/")
 async def read_blocks():
     return "Hello blocks!"
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    data = await websocket.receive_text()
+    subject = Subject()
+
+    def destruct():
+        subject.on_completed()
+        subject.dispose()
+        websocket.close()
+
+    subject.subscribe(on_next=lambda x: websocket.send_text(x), on_error=lambda e: print(e), on_completed=destruct)
--- src/renderer/src/routes/index/Index.tsx
+++ src/renderer/src/routes/index/Index.tsx
@@ -1,7 +1,47 @@
+import {useCallback, useEffect, useState} from "react";
+import useWebSocket, {ReadyState} from "react-use-websocket";
+
 export const Index = (): JSX.Element => {
+  const socketURL = 'http://localhost:2333/blocks/ws'
+  const [messageHistory, setMessageHistory] = useState<any>([])
+  const {sendMessage, lastMessage, readyState} = useWebSocket(socketURL)
+
+
+
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+
+  const handleClickSendMessage = useCallback(() => sendMessage('1'), []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 1
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
+      </ul>
     </div>
-  )
+  );
+
 }
