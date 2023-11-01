6bf36476419f4adbb96b7344adaa474b9ca3ee69
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-01 15:25:27 -0400

    format

--- captain/routers/blocks.py
+++ captain/routers/blocks.py
@@ -12,96 +12,101 @@ router = APIRouter(tags=["blocks"], prefix="/blocks")
 
 @router.get("/")
 async def read_blocks():
-  return "Hello blocks!"
+    return "Hello blocks!"
 
 
 class IgnoreComplete:
-  def __call__(self, source):
-    return create(lambda observer, scheduler: source.subscribe(
-      observer.on_next,
-      lambda err: observer.on_error(err)
-    ))
+    def __call__(self, source):
+        return create(
+            lambda observer, scheduler: source.subscribe(
+                observer.on_next, lambda err: observer.on_error(err)
+            )
+        )
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-  all_events = BehaviorSubject(0)
-  button_events = BehaviorSubject(0)
-  joy_events = BehaviorSubject("axis 0 0")
+    all_events = BehaviorSubject(0)
+    button_events = BehaviorSubject(0)
+    joy_events = BehaviorSubject("axis 0 0")
 
-  def destruct():
-    print("completed")
-    # TODO
+    def destruct():
+        print("completed")
+        # TODO
 
-  def on_next_event(event):
-    print(event)
-    if isinstance(str, event) and event.startswith("axis"):
-      joy_events.on_next(event)
-    else:
-      button_events.on_next(event)
+    def on_next_event(event):
+        print(event)
+        if isinstance(str, event) and event.startswith("axis"):
+            joy_events.on_next(event)
+        else:
+            button_events.on_next(event)
 
-  def on_next_joy(event):
-    x = float(event.split(" ")[-1])
-    print(f"Joy got {event}")
+    def on_next_joy(event):
+        x = float(event.split(" ")[-1])
+        print(f"Joy got {event}")
 
-  def send_button(x) -> Future[None]:
-    return asyncio.ensure_future(websocket.send_text(str(x)))
+    def send_button(x) -> Future[None]:
+        return asyncio.ensure_future(websocket.send_text(str(x)))
 
-  button_events.pipe(IgnoreComplete()).pipe(ops.take_with_time(500), flat_map_latest(send_button)).subscribe(on_next=print, on_error=lambda e: print(e), on_completed=destruct)
-  joy_events.pipe(IgnoreComplete()).subscribe(on_next=on_next_joy, on_error=lambda e: print(e), on_completed=destruct)
+    button_events.pipe(IgnoreComplete()).pipe(
+        ops.take_with_time(500), flat_map_latest(send_button)
+    ).subscribe(on_next=print, on_error=lambda e: print(e), on_completed=destruct)
+    joy_events.pipe(IgnoreComplete()).subscribe(
+        on_next=on_next_joy, on_error=lambda e: print(e), on_completed=destruct
+    )
 
-  all_events.pipe(IgnoreComplete()).subscribe(on_next=on_next_event, on_error=lambda e: print(e), on_completed=destruct)
+    all_events.pipe(IgnoreComplete()).subscribe(
+        on_next=on_next_event, on_error=lambda e: print(e), on_completed=destruct
+    )
 
-  await websocket.accept()
-  while True:
-    data = await websocket.receive_text()
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
 
-    all_events.on_next(data)
-    print(f"Got data: {data}")
-    if data == "close":
-      await websocket.close()
-      break
-
+        all_events.on_next(data)
+        print(f"Got data: {data}")
+        if data == "close":
+            await websocket.close()
+            break
 
 
 flowchart_bootstrap_subject = BehaviorSubject("")
 
 flowchart_display_subject = BehaviorSubject("")
 
+
 @router.websocket("/nodes")
 async def websocket_endpoint_nodes(websocket: WebSocket):
-  send_msg = send_message_factory(websocket)
-  global flowchart_display_subject
-  global flowchart_bootstrap_subject
+    send_msg = send_message_factory(websocket)
+    global flowchart_display_subject
+    global flowchart_bootstrap_subject
 
+    flowchart_bootstrap_subject.pipe(ops.map(lambda x: str(x)[::-1])).subscribe(
+        on_next=lambda x: flowchart_display_subject.on_next(x)
+    )
 
-  flowchart_bootstrap_subject.pipe(ops.map(lambda x: str(x)[::-1])).subscribe(on_next=lambda x: flowchart_display_subject.on_next(x))
+    flowchart_display_subject.pipe(flat_map_latest(send_msg)).subscribe(
+        on_next=print, on_error=lambda e: print(e), on_completed=print("completed")
+    )
 
+    await websocket.accept()
 
-
-  flowchart_display_subject.pipe(flat_map_latest(send_msg)).subscribe(on_next=print, on_error=lambda e: print(e), on_completed=print("completed"))
-
-  await websocket.accept()
-
-  while True:
-    data = await websocket.receive_text()
-    flowchart_bootstrap_subject.on_next(data)
-    print(f"Got data: {data}")
-    if data == "close":
-      await websocket.close()
-      break
-
+    while True:
+        data = await websocket.receive_text()
+        flowchart_bootstrap_subject.on_next(data)
+        print(f"Got data: {data}")
+        if data == "close":
+            await websocket.close()
+            break
 
 
 def send_message_factory(websocket):
-  def send_message(x) -> Future[None]:
-    """
-    USAGE: Flat map to this thingy
-    :param x:
-    :return:
-    """
-    return asyncio.ensure_future(websocket.send_text(str(x)))
+    def send_message(x) -> Future[None]:
+        """
+        USAGE: Flat map to this thingy
+        :param x:
+        :return:
+        """
+        return asyncio.ensure_future(websocket.send_text(str(x)))
 
-  return send_message
-
-
+    return send_message
