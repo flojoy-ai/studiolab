6a708cf27158f7fc29fe82bfd6bd28ea77456702
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-11-01 11:56:47 -0400

    working controller example

--- captain/routers/blocks.py
+++ captain/routers/blocks.py
@@ -1,5 +1,11 @@
+import multiprocessing
+from asyncio import Future
+
 from fastapi import APIRouter, WebSocket
+from reactivex.operators import debounce, flat_map_latest
+from reactivex import operators as ops
 from reactivex.subject import BehaviorSubject
+from reactivex import create
 import asyncio
 
 router = APIRouter(tags=["blocks"], prefix="/blocks")
@@ -7,31 +13,64 @@ router = APIRouter(tags=["blocks"], prefix="/blocks")
 
 @router.get("/")
 async def read_blocks():
-    return "Hello blocks!"
+  return "Hello blocks!"
+
+
+class IgnoreComplete:
+  def __call__(self, source):
+    return create(lambda observer, scheduler: source.subscribe(
+      observer.on_next,
+      lambda err: observer.on_error(err)
+    ))
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    subject = BehaviorSubject(0)
+  all_events = BehaviorSubject(0)
+  button_events = BehaviorSubject(0)
+  joy_events = BehaviorSubject("axis 0 0")
 
-    async def destruct():
-        print("completed")
-        subject.on_completed()
-        subject.dispose()
-        await websocket.close()
+  def destruct():
+    print("completed")
+    # TODO
 
-    async def on_next(x: str):
-        print(x)
-        await websocket.send_text(x)
+  def on_next_event(event):
+    print(event)
+    if type(event) == str and event.startswith("axis"):
+      joy_events.on_next(event)
+    else:
+      button_events.on_next(event)
+
+  def on_next_joy(event):
+    x = float(event.split(" ")[-1])
+    print(f"Joy got {event}")
+
+  def send_button(x) -> Future[None]:
+    return asyncio.ensure_future(websocket.send_text(str(x)))
+
+  button_events.pipe(IgnoreComplete()).pipe(ops.take_with_time(500), flat_map_latest(send_button)).subscribe(on_next=print, on_error=lambda e: print(e), on_completed=destruct)
+  joy_events.pipe(IgnoreComplete()).subscribe(on_next=on_next_joy, on_error=lambda e: print(e), on_completed=destruct)
+
+  all_events.pipe(IgnoreComplete()).subscribe(on_next=on_next_event, on_error=lambda e: print(e), on_completed=destruct)
+
+  await websocket.accept()
+  while True:
+    data = await websocket.receive_text()
+
+    all_events.on_next(data)
+    print(f"Got data: {data}")
+    if data == "close":
+      await websocket.close()
+      break
 
 
-    subject.subscribe(on_next=lambda x: run_sync(on_next(str(x))), on_error=lambda e: print(e), on_completed=lambda: run_sync(destruct))
-    while True:
-        data = await websocket.receive_text()
-        subject.on_next(int(data))
+def send_message_factory(websocket):
+  def send_message(x) -> Future[None]:
+    """
+    USAGE: Flat map to this thingy
+    :param x:
+    :return:
+    """
+    return asyncio.ensure_future(websocket.send_text(str(x)))
 
-
-def run_sync(func):
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(asyncio.create_task(func))
+  return send_message
--- src/renderer/src/routes/index/Index.tsx
+++ src/renderer/src/routes/index/Index.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
+import { GamepadListener } from 'gamepad.js';
 
 export const Index = (): JSX.Element => {
   const socketURL = 'ws://localhost:2333/blocks/ws'
@@ -22,6 +23,38 @@ export const Index = (): JSX.Element => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated'
   }[readyState]
 
+  let lastMSG = 0;
+
+  const onButtonChange = (button): void => {
+    console.dir(button)
+    if (!button.detail.pressed && (window.performance.now() - lastMSG) > 100) {
+      lastMSG = window.performance.now();
+      sendMessage(button.detail.button);
+    }
+  }
+
+  const listener = new GamepadListener({
+    button: {
+      analog: true
+    }
+  });
+
+  listener.on('gamepad:button', onButtonChange);
+
+  listener.on('gamepad:axis', event => {
+    const {
+      // index,// Gamepad index: Number [0-3].
+      axis, // Axis index: Number [0-N].
+      value, // Current value: Number between -1 and 1. Float in analog mode, integer otherwise.
+      // gamepad, // Native Gamepad object
+    } = event.detail;
+    if (axis === 2){
+      sendMessage(`axis ${axis} ${value}`);
+    }
+  });
+
+  listener.start();
+
   return (
     <div>
       <button onClick={handleClickSendMessage} disabled={readyState !== ReadyState.OPEN}>
@@ -29,11 +62,6 @@ export const Index = (): JSX.Element => {
       </button>
       <span>The WebSocket is currently {connectionStatus}</span>
       {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-      <ul>
-        {messageHistory.map((message, idx) => (
-          <span key={idx}>{message ? message.data : null}</span>
-        ))}
-      </ul>
     </div>
   )
 }
