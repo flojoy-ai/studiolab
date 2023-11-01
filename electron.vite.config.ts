0e3f08b66470a370ea6cffb328868f3a46d4055d
Joey Yu <joey@jyu.dev>
2023-10-31 22:23:19 -0400

    add some ui components and adjust alias

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@': resolve('./src/renderer/src')
       }
     },
     plugins: [react()]
