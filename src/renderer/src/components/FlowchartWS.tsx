edded8d712045246a46c0c74f41d8c9d63ba7243
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-11-03 02:00:28 -0400

    UI is not as ugly and it works well now

--- src/renderer/src/components/FlowchartWS.tsx
+++ src/renderer/src/components/FlowchartWS.tsx
@@ -1,5 +1,9 @@
 import { FC, useEffect, useState } from 'react'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
+import { Button } from './ui/Button'
+import { Separator } from './ui/Separator'
+import { Card } from './ui/Card'
+import { ScrollArea } from './ui/ScrollArea'
 
 export const FlowchartWS: FC = () => {
   const socketURL = 'ws://localhost:2333/blocks/flowchart'
@@ -51,50 +55,56 @@ export const FlowchartWS: FC = () => {
 
   return (
     <>
-      <div>
-        <button onClick={handleStart} disabled={readyState !== ReadyState.OPEN}>
+      <ScrollArea>
+        <span>The WebSocket is currently {connectionStatus}</span>
+        {lastMessage ? <span> | Last message: {lastMessage.data}</span> : null}
+        <Separator />
+        <Button onClick={handleStart} disabled={readyState !== ReadyState.OPEN || adder !== undefined}>
           Start Flowchart
-        </button>
-        <label htmlFor="file">Upload a flowchart</label>
+        </Button>
         <input
           type="file"
           accept={'.json'}
-          onChange={(event) => {
-            const file = event.target.files[0]
+          onChange={(event): void => {
+            const file = event!.target!.files![0]
             const reader = new FileReader()
-            reader.onload = (event) => {
-              const text = event.target.result
+            reader.onload = (event): void  => {
+              const text = event!.target!.result
               setAdder(text as string)
             }
             reader.readAsText(file)
           }}
         ></input>
-        <span>The WebSocket is currently {connectionStatus}</span>
-        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-        {adder !== undefined && adder.blocks.map((block) => {
-          if (block.block_type === 'slider') {
-            return (
-              <div key={block.id}>
-                <input
-                  type="range"
-                  min="0"
-                  max="100"
-                  onChange={(event) => handleSetSlider(event, block.id)}
-                />
-                <pre>
-                  <code>{JSON.stringify(curFC.blocks.find((b) => b.id === block.id) || {})}</code>
-                </pre>
-              </div>
-            )
-          } else {
-            return (
-              <pre key={block.id}>
-                <code>{JSON.stringify(curFC.blocks.find((b) => b.id === block.id) || {})}</code>
-              </pre>
-            )
-          }
-        })}
-      </div>
+        <Separator />
+        {adder !== undefined &&
+          adder.blocks.map((block) => {
+            if (block.block_type === 'slider') {
+              return (
+                <Card key={block.id}>
+                  <h4>{block.id}</h4>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    onChange={(event) => handleSetSlider(event, block.id)}
+                  />
+                  <pre>
+                    <code>{JSON.stringify(curFC.blocks.find((b) => b.id === block.id) || {})}</code>
+                  </pre>
+                </Card>
+              )
+            } else {
+              return (
+                <Card key={block.id}>
+                  <h4>{block.id}</h4>
+                  <pre>
+                    <code>{JSON.stringify(curFC.blocks.find((b) => b.id === block.id) || {})}</code>
+                  </pre>
+                </Card>
+              )
+            }
+          })}
+      </ScrollArea>
     </>
   )
 }
