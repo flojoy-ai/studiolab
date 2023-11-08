6deac949621068e67b8780065bbb26e26754779b
Joey Yu <joey@jyu.dev>
2023-11-07 22:20:53 -0500

    fix style a bit

--- src/renderer/src/components/index/SetupStep.tsx
+++ src/renderer/src/components/index/SetupStep.tsx
@@ -3,11 +3,13 @@ import { CheckCircle, CircleDashed, CircleDotDashed, XCircle } from 'lucide-reac
 
 const SetupStep = ({ status, message }: Omit<SetupStatus, 'stage'>): JSX.Element => {
   return (
-    <div className="flex gap-2 p-2">
-      {status === 'running' && <CircleDotDashed className="animate-spin" />}
-      {status === 'completed' && <CheckCircle />}
-      {status === 'pending' && <CircleDashed />}
-      {status === 'error' && <XCircle />}
+    <div className="flex items-center gap-2 p-2">
+      <div>
+        {status === 'running' && <CircleDotDashed className="animate-spin" />}
+        {status === 'completed' && <CheckCircle />}
+        {status === 'pending' && <CircleDashed />}
+        {status === 'error' && <XCircle />}
+      </div>
       <div>{message}</div>
     </div>
   );
