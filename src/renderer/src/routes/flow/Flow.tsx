20d84a0ef8523dd746682b7d987988407180cf6f
Joey Yu <joey@jyu.dev>
2023-11-10 17:24:33 -0500

    minor tweaking

--- src/renderer/src/routes/flow/Flow.tsx
+++ src/renderer/src/routes/flow/Flow.tsx
@@ -1,26 +1,40 @@
 import BlockLibrary from '@/components/flow/BlockLibrary';
 import FlowCanvas from '@/components/flow/FlowCanvas';
 
-import { Mosaic } from 'react-mosaic-component';
+import { Mosaic, MosaicWindow } from 'react-mosaic-component';
 import 'react-mosaic-component/react-mosaic-component.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 const Flow = (): JSX.Element => {
-  const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
-    a: <BlockLibrary />,
-    b: <FlowCanvas />
+  const TITLE_MAP: Record<ViewId, string> = {
+    block_library: 'Block Library',
+    flow_canvas: 'Flow Canvas'
+  };
+
+  type ViewId = 'block_library' | 'flow_canvas';
+
+  const ELEMENT_MAP: Record<ViewId, JSX.Element> = {
+    block_library: <BlockLibrary />,
+    flow_canvas: <FlowCanvas />
   };
 
   return (
     <div className="main-content flex rounded-lg bg-muted p-4">
-      <Mosaic<string>
-        renderTile={(id) => ELEMENT_MAP[id]}
+      <Mosaic<ViewId>
+        renderTile={(id, path) => (
+          <MosaicWindow<ViewId>
+            path={path}
+            createNode={() => 'block_library'}
+            title={TITLE_MAP[id]}
+          >
+            {ELEMENT_MAP[id]}
+          </MosaicWindow>
+        )}
         initialValue={{
           direction: 'row',
-          first: 'a',
-          second: 'b',
-          splitPercentage: 40
+          first: 'block_library',
+          second: 'flow_canvas'
         }}
       />
     </div>
