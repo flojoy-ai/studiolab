f33b60b51b9550378c53bebe1b06adfa2b179a0a
Joey Yu <joey@jyu.dev>
2023-11-09 21:15:49 -0500

    add an example node for positioning

--- src/renderer/src/routes/flow/Flow.tsx
+++ src/renderer/src/routes/flow/Flow.tsx
@@ -2,9 +2,24 @@ import ReactFlow, { Controls, Background, MiniMap } from 'reactflow';
 import 'reactflow/dist/style.css';
 
 const Flow = (): JSX.Element => {
+  const edges = [{ id: '1-2', source: '1', target: '2' }];
+
+  const nodes = [
+    {
+      id: '1',
+      data: { label: 'Hello' },
+      position: { x: 0, y: 0 },
+      type: 'input'
+    },
+    {
+      id: '2',
+      data: { label: 'World' },
+      position: { x: 100, y: 100 }
+    }
+  ];
   return (
     <div className="main-content m-4 flex grow flex-col items-center rounded-lg bg-background">
-      <ReactFlow proOptions={{ hideAttribution: true }}>
+      <ReactFlow proOptions={{ hideAttribution: true }} nodes={nodes} edges={edges}>
         <Background />
         <Controls />
         <MiniMap />
