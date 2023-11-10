28718158b07e7d600bb24b81d8be88b0cb44c5e7
Joey Yu <joey@jyu.dev>
2023-11-10 13:50:42 -0500

    add block button

--- src/renderer/src/components/flow/FlowControlsTopLeft.tsx
+++ src/renderer/src/components/flow/FlowControlsTopLeft.tsx
@@ -1,9 +1,23 @@
+import { UIState, useUIStateStore } from '@/stores/ui';
+import { useShallow } from 'zustand/react/shallow';
 import { Button } from '../ui/Button';
 
 const FlowControlsTopLeft = (): JSX.Element => {
+  const { isBlocksLibraryActive, setIsBlocksLibraryActive } = useUIStateStore(
+    useShallow((state: UIState) => ({
+      isBlocksLibraryActive: state.isBlocksLibraryActive,
+      setIsBlocksLibraryActive: state.setIsBlocksLibraryActive
+    }))
+  );
   return (
     <div className="absolute z-50 flex gap-2 p-4">
-      <Button>Add Blocks</Button>
+      <Button
+        onClick={(): void => {
+          setIsBlocksLibraryActive(!isBlocksLibraryActive);
+        }}
+      >
+        Add Blocks
+      </Button>
     </div>
   );
 };
--- src/renderer/src/routes/flow/Flow.tsx
+++ src/renderer/src/routes/flow/Flow.tsx
@@ -1,9 +1,12 @@
 import BlockCard from '@/components/flow/BlockCard';
 import FlowControlsTopLeft from '@/components/flow/FlowControlsTopLeft';
 import FlowControlsTopRight from '@/components/flow/FlowControlsTopRight';
+import { UIState, useUIStateStore } from '@/stores/ui';
+import { cn } from '@/utils/style';
 import SmartBezierEdge from '@tisoap/react-flow-smart-edge';
 import ReactFlow, { Controls, Background, MiniMap } from 'reactflow';
 import 'reactflow/dist/style.css';
+import { useShallow } from 'zustand/react/shallow';
 
 const Flow = (): JSX.Element => {
   const edges = [{ id: '1-2', source: '1', target: '2' }];
@@ -28,21 +31,38 @@ const Flow = (): JSX.Element => {
     smart: SmartBezierEdge
   };
 
+  const { isBlocksLibraryActive } = useUIStateStore(
+    useShallow((state: UIState) => ({
+      isBlocksLibraryActive: state.isBlocksLibraryActive
+    }))
+  );
+
   return (
-    <div className="main-content flex gap-4 rounded-lg bg-muted p-4">
-      <div className="w-96 rounded-lg bg-background">
-        <div className="p-4 text-2xl font-bold">Blocks Library</div>
-        <div className="flex flex-col gap-2 p-4">
-          <BlockCard name="Add" desc="Add a bunch of stuff together" />
-          <BlockCard name="Subtract" desc="Subtract a bunch of stuff together" />
-          <BlockCard name="Debug" desc="Debugggg" />
+    <div className={'main-content flex  rounded-lg bg-muted p-4'}>
+      <div
+        className={cn('rounded-lg bg-background transition-all duration-100 ease-in-out', {
+          'mr-4 w-96 delay-0': isBlocksLibraryActive,
+          'w-0 delay-100': !isBlocksLibraryActive
+        })}
+      >
+        <div
+          className={cn('rounded-lg bg-background transition-all duration-100 ease-in-out', {
+            'opacity-100 delay-100': isBlocksLibraryActive,
+            'opacity-0 delay-0': !isBlocksLibraryActive
+          })}
+        >
+          <div className="p-4 text-2xl font-bold">Blocks Library</div>
+          <div className="flex flex-col gap-2 p-4">
+            <BlockCard name="Add" desc="Add a bunch of stuff together" />
+            <BlockCard name="Subtract" desc="Subtract a bunch of stuff together" />
+            <BlockCard name="Debug" desc="Debugggg" />
+          </div>
         </div>
       </div>
       <ReactFlow
         proOptions={{ hideAttribution: true }}
         nodes={nodes}
         edges={edges}
-        draggable={true}
         edgeTypes={edgeTypes}
         className="rounded-lg bg-background"
       >
