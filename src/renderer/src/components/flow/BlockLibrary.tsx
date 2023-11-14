8a84463d137164ea9f8616c85c8469c2c9fd41bc
Joey Yu <joey@jyu.dev>
2023-11-14 15:55:37 -0500

    remove mosaic for now

--- src/renderer/src/components/flow/BlockLibrary.tsx
+++ src/renderer/src/components/flow/BlockLibrary.tsx
@@ -3,6 +3,8 @@ import 'reactflow/dist/style.css';
 
 import { useShallow } from 'zustand/react/shallow';
 import { useFlowchartStore } from '@/stores/flowchart';
+import { useUIStateStore } from '@/stores/ui';
+import { cn } from '@/utils/style';
 const BlockLibrary = () => {
   const { addNode } = useFlowchartStore(
     useShallow((state) => ({
@@ -10,17 +12,29 @@ const BlockLibrary = () => {
     }))
   );
 
+  const { isBlocksLibraryActive } = useUIStateStore((state) => ({
+    isBlocksLibraryActive: state.isBlocksLibraryActive
+  }));
+
   const handleAddSlider = addNode('slider');
   const handleAddAdd = addNode('add');
   const handleAddBigNumber = addNode('bignum');
 
   return (
-    <div className="h-full bg-background">
-      <div className="p-4 text-2xl font-bold">Blocks Library</div>
-      <div className="flex flex-col gap-2 p-4">
-        <BlockCard name="Add" desc="Add a bunch of stuff together" onClick={handleAddAdd} />
-        <BlockCard name="Slider" desc="it slides" onClick={handleAddSlider} />
-        <BlockCard name="Big Number" desc="Big number" onClick={handleAddBigNumber} />
+    <div
+      className={cn('flex transition-all delay-150 duration-150', {
+        'w-96': isBlocksLibraryActive,
+        'w-0': !isBlocksLibraryActive
+      })}
+    >
+      <div className="px-2"></div>
+      <div className="grow flex-col rounded-lg bg-background">
+        <div className="p-4 text-2xl font-bold">Blocks Library</div>
+        <div className="flex flex-col gap-2 p-4">
+          <BlockCard name="Add" desc="Add a bunch of stuff together" onClick={handleAddAdd} />
+          <BlockCard name="Slider" desc="it slides" onClick={handleAddSlider} />
+          <BlockCard name="Big Number" desc="Big number" onClick={handleAddBigNumber} />
+        </div>
       </div>
     </div>
   );
--- src/renderer/src/routes/flow/Flow.tsx
+++ src/renderer/src/routes/flow/Flow.tsx
@@ -1,50 +1,13 @@
 import BlockLibrary from '@/components/flow/BlockLibrary';
 import FlowCanvas from '@/components/flow/FlowCanvas';
 
-import { Mosaic, MosaicWindow } from 'react-mosaic-component';
-import 'react-mosaic-component/react-mosaic-component.css';
-// TODO: this breaks some style, fix later
-// import '@blueprintjs/core/lib/css/blueprint.css';
-// import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import Editor from '@/components/editor/Editor';
-
 const Flow = (): JSX.Element => {
-  const TITLE_MAP: Record<ViewId, string> = {
-    block_library: 'Block Library',
-    flow_canvas: 'Flow Canvas',
-    code_editor: 'Code Editor'
-  };
-
-  type ViewId = 'block_library' | 'flow_canvas' | 'code_editor';
-
-  const ELEMENT_MAP: Record<ViewId, JSX.Element> = {
-    block_library: <BlockLibrary />,
-    flow_canvas: <FlowCanvas />,
-    code_editor: <Editor />
-  };
-
   return (
-    <div className="main-content flex rounded-lg bg-muted p-4">
-      <Mosaic<ViewId>
-        renderTile={(id, path) => (
-          <MosaicWindow<ViewId>
-            path={path}
-            createNode={() => 'block_library'}
-            title={TITLE_MAP[id]}
-          >
-            {ELEMENT_MAP[id]}
-          </MosaicWindow>
-        )}
-        initialValue={{
-          direction: 'row',
-          first: 'block_library',
-          second: {
-            direction: 'column',
-            first: 'flow_canvas',
-            second: 'code_editor'
-          }
-        }}
-      />
+    <div className="main-content flex  bg-muted py-4">
+      <BlockLibrary />
+      <div className="px-2"></div>
+      <FlowCanvas />
+      <div className="px-2"></div>
     </div>
   );
 };
