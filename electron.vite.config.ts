57294be545065412e91abb010ff7f10e6ed14b44
Joey Yu <joey@jyu.dev>
2023-11-27 21:01:54 -0500

    build: fix build for gRPC

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      preserveSymlinks: true // required for gRPC to work
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
