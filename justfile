599cd51446c75080ed681af1bcca0399950d41fc
Joey Yu <joey@jyu.dev>
2023-11-27 21:41:17 -0500

    build: gen js and ts

--- justfile
+++ justfile
@@ -1,3 +1,5 @@
+PROTO_DEST := './protos'
+
 proto:
   just proto-py & just proto-js
 
@@ -7,6 +9,16 @@ proto-py:
 
 proto-js:
   pnpm exec grpc_tools_node_protoc \
+    --js_out=import_style=commonjs,binary:{{PROTO_DEST}} \
+    --grpc_out={{PROTO_DEST}} \
+    --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
+    -I ./protos \
+    protos/*.proto
+
+proto-ts:
+  pnpm exec grpc_tools_node_protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=. \\
-    --grpc_out=grpc_js:. protos/hello.proto
+    --ts_out={{PROTO_DEST}} \
+    -I ./protos \
+    protos/*.proto
+
--- protos/hello_grpc_pb.js
+++ protos/hello_grpc_pb.js
@@ -1,29 +1,29 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 'use strict';
-var grpc = require('@grpc/grpc-js');
-var protos_hello_pb = require('../protos/hello_pb.js');
+var grpc = require('grpc');
+var hello_pb = require('./hello_pb.js');
 
 function serialize_hello_HelloReply(arg) {
-  if (!(arg instanceof protos_hello_pb.HelloReply)) {
+  if (!(arg instanceof hello_pb.HelloReply)) {
     throw new Error('Expected argument of type hello.HelloReply');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_hello_HelloReply(buffer_arg) {
-  return protos_hello_pb.HelloReply.deserializeBinary(new Uint8Array(buffer_arg));
+  return hello_pb.HelloReply.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 function serialize_hello_HelloRequest(arg) {
-  if (!(arg instanceof protos_hello_pb.HelloRequest)) {
+  if (!(arg instanceof hello_pb.HelloRequest)) {
     throw new Error('Expected argument of type hello.HelloRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_hello_HelloRequest(buffer_arg) {
-  return protos_hello_pb.HelloRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return hello_pb.HelloRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 
@@ -34,8 +34,8 @@ sayHello: {
     path: '/hello.Greeter/SayHello',
     requestStream: false,
     responseStream: false,
-    requestType: protos_hello_pb.HelloRequest,
-    responseType: protos_hello_pb.HelloReply,
+    requestType: hello_pb.HelloRequest,
+    responseType: hello_pb.HelloReply,
     requestSerialize: serialize_hello_HelloRequest,
     requestDeserialize: deserialize_hello_HelloRequest,
     responseSerialize: serialize_hello_HelloReply,
@@ -46,8 +46,8 @@ sayHelloAgain: {
     path: '/hello.Greeter/SayHelloAgain',
     requestStream: false,
     responseStream: false,
-    requestType: protos_hello_pb.HelloRequest,
-    responseType: protos_hello_pb.HelloReply,
+    requestType: hello_pb.HelloRequest,
+    responseType: hello_pb.HelloReply,
     requestSerialize: serialize_hello_HelloRequest,
     requestDeserialize: deserialize_hello_HelloRequest,
     responseSerialize: serialize_hello_HelloReply,
