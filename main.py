a0c20d888fa89c3e14121d907b3a04057129cfd5
Joey Yu <joey@jyu.dev>
2023-11-27 22:14:04 -0500

    wip: not working yeet cuz typescript

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from concurrent import futures
+
+import grpc
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from captain.routers import blocks, status
+from protos import hello_pb2, hello_pb2_grpc
 
 app = FastAPI()
 
@@ -23,6 +27,22 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    import uvicorn
+    # import uvicorn
+    #
+    # uvicorn.run("main:app", port=2333, reload=True)
 
-    uvicorn.run("main:app", port=2333, reload=True)
+    class Greeter(hello_pb2_grpc.GreeterServicer):
+        def SayHello(self, request, context):
+            print("yooooo sup", flush=True)
+            return hello_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+    def serve():
+        port = "2333"
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+        server.add_insecure_port("[::]:" + port)
+        server.start()
+        print("Server started, listening on " + port, flush=True)
+        server.wait_for_termination()
+
+    serve()
--- src/main/api/routes/base.ts
+++ src/main/api/routes/base.ts
@@ -3,6 +3,15 @@ import { app } from 'electron';
 import { spawnBlocksLibraryWindow } from '../../windows';
 import { t } from '../trpc';
 
+import grpc from '@grpc/grpc-js';
+import log from 'electron-log/main';
+
+import { HelloRequest } from '../../../../protos/hello_pb';
+import { GreeterClient } from '../../../../protos/hello_grpc_pb';
+
+const client = new GreeterClient('localhost:2333', grpc.credentials.createInsecure());
+const request = new HelloRequest();
+
 export const baseRouter = t.router({
   openLogFolder: t.procedure.mutation(() => {
     openLogFolder();
@@ -13,5 +22,10 @@ export const baseRouter = t.router({
   }),
   spawnBlockLibraryWindow: t.procedure.mutation(async () => {
     await spawnBlocksLibraryWindow();
+  }),
+  checkHealth: t.procedure.query(async () => {
+    client.sayHello(request, function (_, response) {
+      log.info('Greeting:', response.getMessage());
+    });
   })
 });
