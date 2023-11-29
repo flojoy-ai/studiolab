382683c20c277902e863d98ed698dadf511fed13
Joey Yu <joey@jyu.dev>
2023-11-28 19:53:14 -0500

    chore: more cleaning

--- src/renderer/src/components/control/ControlCanvas.tsx
+++ src/renderer/src/components/control/ControlCanvas.tsx
@@ -1,6 +1,3 @@
-// import FlowControlsTopLeft from '@/components/flow/FlowControlsTopLeft';
-// import FlowControlsTopRight from '@/components/flow/FlowControlsTopRight';
-// import SmartBezierEdge from '@tisoap/react-flow-smart-edge';
 import ReactFlow, {
   Controls,
   Background,
@@ -8,7 +5,6 @@ import ReactFlow, {
   // NodeDragHandler,
   // SelectionDragHandler,
   // OnNodesDelete,
-  // OnEdgesDelete,
   // ReactFlowInstance
 } from 'reactflow';
 import 'reactflow/dist/style.css';
@@ -18,23 +14,19 @@ import { nodeTypes } from '@/configs/control';
 // import { useCallback, useState } from 'react';
 // import useUndoRedo from '@/hooks/useUndoRedo';
 
-//
-// const edgeTypes = {
-//   smart: SmartBezierEdge
-// };
-
 const ControlCanvas = () => {
   // const [reactFlowInstance, setReactFlowInstance] = useState<undefined | ReactFlowInstance>(
   //   undefined
   // );
 
   // const { takeSnapshot } = useUndoRedo();
+
   const { controls } = useFlowchartStore(
     useShallow((state) => ({
       controls: state.controls
     }))
   );
-  //
+
   // const onNodeDragStart: NodeDragHandler = useCallback(() => {
   //   // 👇 make dragging a node undoable
   //   takeSnapshot();
@@ -50,68 +42,20 @@ const ControlCanvas = () => {
   //   // 👇 make deleting nodes undoable
   //   takeSnapshot();
   // }, [takeSnapshot]);
-  //
-  // const onEdgesDelete: OnEdgesDelete = useCallback(() => {
-  //   // 👇 make deleting edges undoable
-  //   takeSnapshot();
-  // }, [takeSnapshot]);
-  //
-  // const onDragOver = useCallback((event) => {
-  //   event.preventDefault();
-  //   event.dataTransfer.dropEffect = 'move';
-  // }, []);
-  //
-  // const onDrop = useCallback(
-  //   (event) => {
-  //     event.preventDefault();
-  //
-  //     if (reactFlowInstance === undefined) {
-  //       // this should never happen
-  //       alert('onDrop: React Flow instance is not available!');
-  //       return;
-  //     }
-  //
-  //     const block_id = event.dataTransfer.getData('application/reactflow');
-  //
-  //     // check if the dropped element is valid
-  //     if (typeof block_id === 'undefined' || !block_id) {
-  //       return;
-  //     }
-  //
-  //     // reactFlowInstance.project was renamed to reactFlowInstance.screenToFlowPosition
-  //     // and you don't need to subtract the reactFlowBounds.left/top anymore
-  //     // details: https://reactflow.dev/whats-new/2023-11-10
-  //     const position = reactFlowInstance.screenToFlowPosition({
-  //       x: event.clientX,
-  //       y: event.clientY
-  //     });
-  //
-  //     addNode(block_id, position);
-  //   },
-  //   [reactFlowInstance]
-  // );
 
   return (
     <ReactFlow
       nodes={controls}
       nodeTypes={nodeTypes}
-      // edges={edges}
-      // edgeTypes={edgeTypes}
       // onNodesChange={onNodesChange}
-      // onEdgesChange={onEdgesChange}
       // onConnect={onConnect}
       // onNodeDragStart={onNodeDragStart}
       // onSelectionDragStart={onSelectionDragStart}
       // onNodesDelete={onNodesDelete}
-      // onEdgesDelete={onEdgesDelete}
-      // onDragOver={onDragOver}
-      // onDrop={onDrop}
       // onInit={setReactFlowInstance}
       proOptions={{ hideAttribution: true }}
       className="rounded-lg bg-background"
     >
-      {/* <FlowControlsTopLeft /> */}
-      {/* <FlowControlsTopRight /> */}
       <Background />
       <Controls />
       <MiniMap />
