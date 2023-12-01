919967686948c83aaff40bcdec84a303a8c7be7b
Joey Yu <joey@jyu.dev>
2023-12-01 12:22:42 -0500

    feat: configure electron-updater

--- src/main/api/routes/base.ts
+++ src/main/api/routes/base.ts
@@ -8,6 +8,7 @@ import {
 } from '../../windows';
 import { t } from '../trpc';
 import { z } from 'zod';
+import { autoUpdater } from 'electron-updater';
 
 export const baseRouter = t.router({
   openLogFolder: t.procedure.mutation(() => {
@@ -31,5 +32,8 @@ export const baseRouter = t.router({
   }),
   setTheme: t.procedure.input(z.enum(['light', 'dark', 'system'])).mutation((opts) => {
     nativeTheme.themeSource = opts.input;
+  }),
+  checkForUpdatesAndNotify: t.procedure.mutation(async () => {
+    await autoUpdater.checkForUpdatesAndNotify();
   })
 });
