122e528278a0f07ee5bbb36dfb41b3a5f8058ee4
JeffDotPng <zhangjeff2014@gmail.com>
2023-12-27 11:21:35 -0400

    simplify trpc routers

--- src/main/api/routes/base.ts
+++ src/main/api/routes/base.ts
@@ -11,25 +11,15 @@ import { z } from 'zod';
 import { autoUpdater } from 'electron-updater';
 
 export const baseRouter = t.router({
-  openLogFolder: t.procedure.mutation(() => {
-    openLogFolder();
-  }),
+  openLogFolder: t.procedure.mutation(openLogFolder),
   restartFlojoyStudio: t.procedure.mutation(() => {
     app.relaunch();
     app.exit();
   }),
-  spawnBlockLibraryWindow: t.procedure.mutation(async () => {
-    await spawnBlocksLibraryWindow();
-  }),
-  spawnFlowWindow: t.procedure.mutation(async () => {
-    await spawnFlowWindow();
-  }),
-  spawnControlWindow: t.procedure.mutation(async () => {
-    await spawnControlWindow();
-  }),
-  closeSetupWindow: t.procedure.mutation(async () => {
-    await closeSetupWindow();
-  }),
+  spawnBlockLibraryWindow: t.procedure.mutation(spawnBlocksLibraryWindow),
+  spawnFlowWindow: t.procedure.mutation(spawnFlowWindow),
+  spawnControlWindow: t.procedure.mutation(spawnControlWindow),
+  closeSetupWindow: t.procedure.mutation(closeSetupWindow),
   setTheme: t.procedure.input(z.enum(['light', 'dark', 'system'])).mutation((opts) => {
     nativeTheme.themeSource = opts.input;
   }),
